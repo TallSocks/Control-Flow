ca8346ec2f629a372ebc78489cccc682fe0daa0c
stel <smcarahaly@gmail.com>
2021-10-13 09:51:03 -0700

    first name, last name, pin variables
    declared our first last name variables and tested that they work in a print statement by concatenating in a print statement

--- Control Flow.py
+++ Control Flow.py
@@ -1,4 +1,22 @@
 # Programmer: Stel Carahaly
 # Date: 10.11.21
-# Program: Control Flow
+# Program: ATM Bank Transaction
 
+"""
+This Program simulates an ATM utilizing if elif and else statements
+Nesting If statements and refresh our comparison and logical operators
+"""
+
+print("Welcome to Cash-R-Us Bank\nLet's take a moment to set up your account\n")
+
+# Set up account by asking users for first and last names using Variables
+firstName = input("What is your first name: ")
+lastName = input("What is your last name: ")
+print("\nWelcome to Cash-R-Us,", firstName,lastName + ", we will now set up a security pin on your account. \n")
+pin = input("Please choose a 4 digit security pin: ")
+
+print("\n*************************************************\n")
+"""
+this part of the program will be simulation walking up to the ATM after setting up an account
+this is where the if elif and else statements will be used
+"""
